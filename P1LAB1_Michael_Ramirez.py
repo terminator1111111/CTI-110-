01853c91cf2c52b231b866be3c5434caba56b738
terminator1111111 <ramirezm4023@student.faytechcc.edu>
2026-06-13 20:47:48 -0400

    Add files via upload

---
+++ P1LAB1_Michael_Ramirez.py
@@ -0,0 +1,6 @@
+# These are the required fields for P1LAB1
+name = input("Enter your name: ")
+date = input("Enter the Date: ")
+assignment_name = input("Enter your Assignment Name: ")
+project_description = input("Enter a brief description of the project: ")
+print("Hello, " + name + "! Welcome to CTI-110 Online! Today's date is " + date +  ", Your Project Details Are: " + project_description + ", Assignment Name: " + assignment_name)
